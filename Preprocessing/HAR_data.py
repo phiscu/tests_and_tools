1ef641c1153c4701763e39038dc9884f54b1d67b
Phillip Schuster <phillip.schuster@posteo.de>
2022-05-12 09:09:58 +0200

    HAR-ERA5 comparison

--- Preprocessing/HAR_data.py
+++ Preprocessing/HAR_data.py
@@ -9,30 +9,38 @@ import numpy as np
 import salem
 from pathlib import Path; home = str(Path.home())
 
-lat = 41; lon = 75.9
-start_date = '2000-01-01'; end_date = '2020-12-31'
+lat = 42.19; lon = 78.2
+start_date = '2007-01-01'; end_date = '2014-12-31'
 
 ##
 shdf = salem.read_shapefile(home+ "/Seafile/Ana-Lena_Phillip/data/input_output/input/observations/karabatkak/static/shapefile_hydro_kyzylsuu.shp")
-ds = salem.open_wrf_dataset(home +"/Desktop/HAR/HARv2_d10km_d_2d_prcp_2000.nc")
-
+ds = salem.open_wrf_dataset(home +"/Seafile/EBA-CA/Tianshan_data/HARv2/variables/all_variables_kyzylsuu_HARv2_2007_2014.nc")
 crop_extent = shdf.to_crs(ds.pyproj_srs)
 crop_extent.crs = ds.pyproj_srs
 
+## Test xarray:
+ds = xr.open_dataset(home + "/Seafile/EBA-CA/Tianshan_data/HARv2/variables/all_variables_kyzylsuu_HARv2_2007_2014.nc")
+
+# STIMMT DAS? SIND DIE KOORDINATEN WIRKLICH IN GRAD? WIRD DIE RICHTIGE GITTERZELLE ANGESPROCHEN?
+pick = ds.sel(south_north=42.191433, west_east=78.200253, method='nearest')           # closest to AWS location
+# pick = pick.sel(time=slice('1989-01-01', '2019-12-31'))                      # start of gauging till end of file
+era = pick.to_dataframe().filter(['t2m', 'tp'])
+
+
 new_shdf = crop_extent.copy()
-#add buffer to geom, else it will not be detected within bounds of 10km spacing
+#add buffer to geom, otherwise not detected within bounds of 10km spacing
 new_shdf.geometry = new_shdf.geometry.buffer(6000)
 
 clipped_ds = ds.salem.subset(shape=new_shdf)
 
-ds_static = salem.open_wrf_dataset('/home/ana/Desktop/HAR/HAR v2_d10km_static_hgt.nc')
+ds_static = salem.open_wrf_dataset(home + '/Seafile/EBA-CA/Tianshan_data/HARv2/static/all_static_kyzylsuu_HARv2_2007_2014.nc')
 clipped_static = ds_static.salem.subset(shape=new_shdf)
 
 fig = plt.figure(figsize=(16,12), dpi=300)
 ax = fig.add_subplot(111)
 shdf.plot(ax=ax, zorder=3)
 new_shdf.plot(alpha=0.2, zorder=2, ax=ax)
-clipped_ds.prcp.mean(dim='time').plot(ax=ax, zorder=-1)
+clipped_ds.prcp_nc.mean(dim='time').plot(ax=ax, zorder=-1)
 plt.show()
 
 ### Convert shapefile to wrf projection ###
