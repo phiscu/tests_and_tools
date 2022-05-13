902fec3e4acf71820cdc35a34f7badc063853c51
Phillip Schuster <phillip.schuster@posteo.de>
2022-05-13 13:34:06 +0200

    Finished gauging Batysh Sook

--- Preprocessing/Field_data_preprocessing/preprocessing_gauging_sook.py
+++ Preprocessing/Field_data_preprocessing/preprocessing_gauging_sook.py
@@ -11,6 +11,8 @@ import scipy.optimize as sp
 sys.path.append('/home/phillip/Seafile/Ana-Lena_Phillip/data/tests_and_tools/Preprocessing/')
 from Downscaling.ERA5_downscaling.fundamental_physical_constants import g, M, R
 from Preprocessing_functions import hour_rounder
+from sklearn.linear_model import LinearRegression
+
 
 home = str(Path.home())
 working_directory = home + '/Seafile/EBA-CA/Tianshan_data/HOBO_water/Batysh_Sook/'
@@ -24,6 +26,23 @@ lat_hobo = 41.799247
 lon_hobo = 77.749729
 alt_aws_far = 3561 # ALOS DEM; GPS: 3552
 lapseT = -0.006
+
+
+## HOBO-Logger:
+hobo = pd.read_csv(working_directory + 'Batysh_Sook_2021_clean.csv', parse_dates=['time_utc'])  # , index_col='time_utc'
+hobo.time_utc = [hour_rounder(i) for i in hobo.time_utc]    # Round measurement times to full hours
+hobo.set_index("time_utc", inplace=True)
+hobo.press = hobo.press / 100       # from Pa to hPa
+
+# Major disruption in April 2020 - best estimate:
+start = '2019-10-01'
+end = '2020-04-16'
+span = slice(start, end)
+
+# Exclude outlier period:
+start_out = '2020-04-16'
+end_out = '2020-05-31'
+outl = slice(start_out, end_out)
 
 
 ## ERA5
@@ -55,31 +74,7 @@ era_hobo.sp = ((era5_land['sp'].values)/100) * (1-abs(lapseT)*height_diff/era5_l
                ((g*M)/(R*abs(lapseT)))
 era_hobo.t2m = (era5_land['t2m'].values) + height_diff * lapseT
 
-## AWS:
-aws_t2m = pd.read_csv(working_directory + 't2m_taragai_2014-2021.csv', parse_dates=['time'], index_col='time')
-aws_t2m = aws_t2m.resample('H').mean()
-aws_tp = pd.read_csv(working_directory + 'tp_taragai_2014-2021.csv', parse_dates=['time'], index_col='time')
-aws_tp = aws_tp.resample('H').sum()
-aws_sp = pd.read_csv(working_directory + 'sp_taragai_2014-2021.csv', parse_dates=['time'], index_col='time')
-aws_sp = aws_sp.resample('H').mean()
-
-aws = pd.concat([aws_t2m, aws_tp, aws_sp], axis = 1)
-
-height_diff = alt_aws_far - alt_hobo
-
-aws['press_hobo'] = (aws.sp) * (1-abs(lapseT)*height_diff/aws.t2m) ** ((g*M)/(R*abs(lapseT)))
-
-
-# RIESENDRUCKUNTERSCHIED UND SEHR MERKWÜRDIGE WERTE! WAS DA LOS?
-
-
-
-
-## HOBO-Logger:
-hobo = pd.read_csv(working_directory + 'Batysh_Sook_2021_clean.csv', parse_dates=['time_utc'])  # , index_col='time_utc'
-hobo.time_utc = [hour_rounder(i) for i in hobo.time_utc]    # Round measurement times to full hours
-hobo.set_index("time_utc", inplace=True)
-hobo.press = hobo.press / 100       # from Pa to hPa
+## Comparison with ERA5:
 
 t = slice(time_start, '2020-12-31')
 
@@ -87,12 +82,29 @@ data = pd.DataFrame({'water_press': hobo['press'][t], 'air_press': era_hobo['sp'
 
 data.plot(); plt.show()
 
-# Major disruption in April 2020 - best estimate:
-start = '2019-10-01'
-end = '2020-04-12'
-span = slice(start, end)
-
 hobo_cut = hobo['press'][span] - era_hobo['sp'][span]
+
+
+
+## AWS:
+aws_t2m = pd.read_csv(working_directory + 't2m_taragai_2014-2021.csv', parse_dates=['time'], index_col='time')
+aws_t2m = aws_t2m.resample('H').mean() + 273.15
+aws_tp = pd.read_csv(working_directory + 'tp_taragai_2014-2021.csv', parse_dates=['time'], index_col='time')
+aws_tp = aws_tp.resample('H').sum()
+aws_sp = pd.read_csv(working_directory + 'sp_taragai_2014-2021.csv', parse_dates=['time'], index_col='time')
+aws_sp = aws_sp.resample('H').mean()
+
+aws = pd.concat([aws_t2m, aws_tp, aws_sp], axis = 1)
+
+height_diff = alt_hobo - alt_aws_far
+
+aws['press_hobo'] = (aws.sp) * (1-abs(lapseT)*height_diff/aws.t2m) ** ((g*M)/(R*abs(lapseT)))
+t = slice(time_start, '2020-12-31')
+
+data = pd.DataFrame({'water_press': hobo['press'][t], 'air_press_aws': aws['sp'][t], 'air_press_aws_scaled': aws['press_hobo'][t]})
+data[outl] = np.NaN
+data.plot(); plt.show()
+
 
 
 ## Calculate stage from water pressure
@@ -100,10 +112,54 @@ hobo_cut = hobo['press'][span] - era_hobo['sp'][span]
 def h(p):  # Water column in meter from hydrostatic pressure in Pa!
     return p / (9.81 * 1000)  # acceleration of gravity: 9.81 m/s², density of water 1000 kg/m³
 
-hobo_cut = hobo['press'][span] - aws['press_hobo'][span]
+data['water_press_diff'] = data.water_press - data.air_press_aws_scaled
 
-hobo['water_column'] = h(hobo.water_press * 100)
+data['stage'] = h(aws.water_press_diff * 100)
 
-hobo.describe()
+plt.plot(data.stage); plt.show()
 
-plt.plot(aws.t2m); plt.show()
+##  Dilution gauging:
+
+gaug = pd.read_csv(working_directory + 'dilution_gauging_batysh_sook_2019.csv', parse_dates=['time_utc'])
+gaug.time_utc = [hour_rounder(i) for i in gaug.time_utc]    # Round measurement times to full hours
+gaug.set_index("time_utc", inplace=True)
+gaug.drop('time', axis=1)
+
+## Predict runoff with stage:
+
+hobo_ro = pd.merge(gaug, data.stage, how='left', left_index=True, right_index=True)
+
+predictor = 'stage'
+X = hobo_ro.loc[:, predictor].values.reshape(-1, 1)
+Y = hobo_ro.loc[:, 'runoff'].values.reshape(-1, 1)
+linear_regressor = LinearRegression()
+linear_regressor.fit(X, Y)
+Y_pred = linear_regressor.predict(X)
+
+plt.scatter(X, Y)
+plt.plot(X, Y_pred, color='red')
+plt.xlabel('Water column above sensor [m]')
+plt.ylabel('Run-off [m³/s')
+plt.show()
+
+print('------ Linear Regression -----')
+print('Function: y = %.3f * x + %.3f' % (linear_regressor.coef_[0], linear_regressor.intercept_))
+print("R² Score: {:.2f}".format(linear_regressor.score(X, Y)))
+
+## Calculate runoff from stage:
+
+runoff = pd.DataFrame({'runoff': linear_regressor.coef_[0] * data.stage + linear_regressor.intercept_})
+# runoff[runoff.index.month.isin([10, 11, 12, 1, 2, 3, 4])] = 0
+runoff[runoff.runoff < 0] = 0
+runoff.describe()
+
+plt.plot(runoff_D_sum)
+plt.ylabel('Run-off [m³/d]')
+plt.show()
+
+runoff_D_mean = runoff.resample('D').mean()
+runoff_D_sum = (runoff * 3600).resample('D').sum()
+runoff_D_mean.to_csv(working_directory + "runoff_sook_from_hobo_daily_means.csv")
+runoff_D_sum.to_csv(working_directory + "runoff_sook_from_hobo_daily_sum.csv")
+runoff.to_csv(working_directory + "runoff_sook_from_hobo.csv")
+
