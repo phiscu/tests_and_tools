ae0db40a7613e3192c365dd0cfd562b952d56d90
Phillip Schuster <phillip.schuster@posteo.de>
2023-03-31 12:56:14 +0200

    Small bug fixes and clean-up in CMIP6 downloader

--- Tools/CMIP6_EE_multi_downloader.py
+++ Tools/CMIP6_EE_multi_downloader.py
@@ -1,6 +1,7 @@
 ##
 import ee
 import geemap
+import pandas as pd
 import logging
 import multiprocessing
 import geopandas as gpd
@@ -10,100 +11,32 @@ import requests
 from retry import retry
 from tqdm import tqdm
 
-
 try:
     ee.Initialize()
 except Exception as e:
     ee.Authenticate()         # authenticate when using GEE for the first time
     ee.Initialize()
-os.chdir("/home/phillip/Seafile/EBA-CA/Repositories/ee_download_test")
 
 """
-This tool downloads daily CMIP6 data from Google Earth Engine using parallel requests.
-It applies an area-weighted averaging of all overlapping grid cells within a provided
-polygon and stores individual years in separate CSV files.
+This tool applies an approach by [Noel Gorelick et. al.](https://gorelick.medium.com/fast-er-downloads-a2abd512aa26)
+to download daily CMIP6 data from Google Earth Engine using parallel requests. It applies an area-weighted averaging 
+to all overlapping grid cells within a provided polygon and stores individual years in separate CSV files.
+The files are then loaded and combined in dataframes.
+
+@author: Phillip Schuster and Alexander Georgi
 """
 
-## CMIPDownloader as a function
+## Path
 
-def download_cmip(var, starty, endy, shape, processes=10):
-
-    def getRequests(starty, endy):
-        """Generates a list of years to be downloaded."""
-        return [i for i in range(starty, endy+1)]
-
-    global getResult
-
-    @retry(tries=10, delay=1, backoff=2)
-    def getResult(index, year):
-        """Handle the HTTP requests to download one year of CMIP6 data."""
-
-        start = str(year) + '-01-01'
-        end = str(year + 1) + '-01-01'
-
-        startDate = ee.Date(start)
-        endDate = ee.Date(end)
-        n = endDate.difference(startDate, 'day').subtract(1)
-
-        def getImageCollection(var):
-            collection = ee.ImageCollection('NASA/GDDP-CMIP6') \
-                .select(var) \
-                .filterDate(startDate, endDate) \
-                .filterBounds(shape)
-            return collection
-
-        def renameBandName(b):
-            split = ee.String(b).split('_')
-            return ee.String(split.splice(split.length().subtract(2), 1).join("_"))
-
-        def buildFeature(i):
-            t1 = startDate.advance(i, 'day')
-            t2 = t1.advance(1, 'day')
-            # feature = ee.Feature(point)
-            dailyColl = collection.filterDate(t1, t2)
-            dailyImg = dailyColl.toBands()
-            # renaming and handling names
-            bands = dailyImg.bandNames()
-            renamed = bands.map(renameBandName)
-            # Daily extraction and adding time information
-            dict = dailyImg.rename(renamed).reduceRegion(
-                reducer=ee.Reducer.mean(),
-                geometry=shape,
-            ).combine(
-                ee.Dictionary({'system:time_start': t1.millis(), 'isodate': t1.format('YYYY-MM-dd')})
-            )
-            return ee.Feature(None, dict)
-
-        # Create features for all days in the respective year
-        collection = getImageCollection(var)
-        year_feature = ee.FeatureCollection(ee.List.sequence(0, n).map(buildFeature))
-
-        # Create a download URL for a CSV containing the feature collection
-        url = year_feature.getDownloadURL()
-
-        # Handle downloading the actual annual csv
-        r = requests.get(url, stream=True)
-        if r.status_code != 200:
-            r.raise_for_status()
-
-        filename = 'cmip6_' + var + '_' + str(year) + '.csv'
-        with open(filename, 'w') as f:
-            f.write(r.text)
-
-        print("Done: ", index)
-
-    logging.basicConfig()
-    items = getRequests(starty, endy)
-
-    pool = multiprocessing.Pool(processes)
-    pool.starmap(getResult, enumerate(items))
-
-    pool.close()
-    pool.join()
+wd = '/home/phillip/Seafile/EBA-CA/Repositories'
 
 
-## CMIPDownloader as a class including a status bar
+## Class to download spatially averaged CMIP6 data vor a given period, variable, and spatial subset.
+
+
 class CMIPDownloader:
+    """Class to download spatially averaged CMIP6 data for a given period, variable, and spatial subset."""
+
     def __init__(self, var, starty, endy, shape, processes=10, dir='./'):
         self.var = var
         self.starty = starty
@@ -117,14 +50,18 @@ class CMIPDownloader:
             os.makedirs(self.directory)
 
     def download(self):
+        """Runs a subset routine for CMIP6 data on GEE servers to create ee.FeatureCollections for all years in
+        the requested period. Downloads individual years in parallel processes to increase the download time."""
 
         def getRequests(starty, endy):
-            """Generates a list of years to be downloaded."""
+            """Generates a list of years to be downloaded. [Client side]"""
+
             return [i for i in range(starty, endy+1)]
 
         @retry(tries=10, delay=1, backoff=2)
         def getResult(index, year):
-            """Handle the HTTP requests to download one year of CMIP6 data."""
+            """Handle the HTTP requests to download one year of CMIP6 data. [Server side]"""
+
             start = str(year) + '-01-01'
             end = str(year + 1) + '-01-01'
             startDate = ee.Date(start)
@@ -132,6 +69,9 @@ class CMIPDownloader:
             n = endDate.difference(startDate, 'day').subtract(1)
 
             def getImageCollection(var):
+                """Create and image collection of CMIP6 data for the requested variable, period, and region.
+                [Server side]"""
+
                 collection = ee.ImageCollection('NASA/GDDP-CMIP6') \
                     .select(var) \
                     .filterDate(startDate, endDate) \
@@ -139,10 +79,15 @@ class CMIPDownloader:
                 return collection
 
             def renameBandName(b):
+                """Edit variable names for better readability. [Server side]"""
+
                 split = ee.String(b).split('_')
                 return ee.String(split.splice(split.length().subtract(2), 1).join("_"))
 
             def buildFeature(i):
+                """Create an area weighted average of the defined region for every day in the given year.
+                [Server side]"""
+
                 t1 = startDate.advance(i, 'day')
                 t2 = t1.advance(1, 'day')
                 # feature = ee.Feature(point)
@@ -160,14 +105,14 @@ class CMIPDownloader:
                 )
                 return ee.Feature(None, dict)
 
-            # Create features for all days in the respective year
+            # Create features for all days in the respective year. [Server side]
             collection = getImageCollection(self.var)
             year_feature = ee.FeatureCollection(ee.List.sequence(0, n).map(buildFeature))
 
-            # Create a download URL for a CSV containing the feature collection
+            # Create a download URL for a CSV containing the feature collection. [Server side]
             url = year_feature.getDownloadURL()
 
-            # Handle downloading the actual annual csv
+            # Handle downloading the actual csv for one year. [Client side]
             r = requests.get(url, stream=True)
             if r.status_code != 200:
                 r.raise_for_status()
@@ -177,9 +122,11 @@ class CMIPDownloader:
 
             return index
 
+        # Create a list of years to be downloaded. [Client side]
         items = getRequests(self.starty, self.endy)
 
-        with tqdm(total=len(items), desc="Downloading CMIP6 data") as pbar:
+        # Launch download requests in parallel processes and display a status bar. [Client side]
+        with tqdm(total=len(items), desc="Downloading CMIP6 data for variable '" + self.var + "'") as pbar:
             results = []
             with concurrent.futures.ThreadPoolExecutor(max_workers=self.processes) as executor:
                 for i, year in enumerate(items):
@@ -190,58 +137,98 @@ class CMIPDownloader:
 
         print("All downloads complete.")
 
-## Application example
 
-output_gpkg = '/home/phillip/Seafile/EBA-CA/Repositories/matilda_edu/output/catchment_data.gpkg'
+## Usage example
+
+output_gpkg = wd + '/matilda_edu/output/catchment_data.gpkg'
 catchment_new = gpd.read_file(output_gpkg, layer='catchment_new')
 catchment = geemap.geopandas_to_ee(catchment_new)
 
-downloader = CMIPDownloader('tas', 1979, 2100, catchment, processes=25, dir='./new_test')
+downloader = CMIPDownloader('tas', 1979, 2100, catchment, processes=25, dir=wd + '/ee_download_test')
 downloader.download()
 
-##
-import pandas as pd
 
-cols = list(pd.read_csv('cmip6_pr_2022.csv', nrows=1))
+## Class to pre-process the downloaded files
+
+class CMIPProcessor:
+    """Class to read and pre-process CSV files downloaded by the CMIPDownloader class."""
+    def __init__(self, var, dir='.'):
+        self.dir = dir
+        self.var = var
+        self.df_hist = self.append_df(self.var, self.dir, hist=True)
+        self.df_ssp = self.append_df(self.var, self.dir, hist=False)
+        self.ssp2_common, self.ssp5_common, self.hist_common,\
+            self.common_models, self.dropped_models = self.process_dataframes()
+        self.ssp2, self.ssp5 = self.get_results()
+
+    def read_cmip(self, filename):
+        """Reads CMIP6 CSV files and drops redundant columns."""
+
+        df = pd.read_csv(filename, index_col='isodate', parse_dates=['isodate'])
+        df = df.drop(['system:index', '.geo', 'system:time_start'], axis=1)
+        return df
+
+    def append_df(self, var, dir='.', hist=True):
+        """Reads CMIP6 CSV files of individual years and concatenates them into dataframes for the full downloaded
+        period. Historical and scenario datasets are treated separately. Drops a model with data gaps.
+        Converts precipitation unit to mm."""
+
+        df_list = []
+        if hist:
+            starty = 1979
+            endy = 2014
+        else:
+            starty = 2015
+            endy = 2100
+        for i in range(starty, endy + 1):
+            filename = dir + '/cmip6_' + var + '_' + str(i) + '.csv'
+            df_list.append(self.read_cmip(filename))
+        if hist:
+            hist_df = pd.concat(df_list).drop('historical_GFDL-CM4_' + var, axis=1)
+            if var == 'pr':
+                hist_df = hist_df * 86400       # from kg/(m^2*s) to mm/day
+            return hist_df
+        else:
+            ssp_df = pd.concat(df_list).drop(['ssp585_GFDL-CM4_' + var, 'ssp245_GFDL-CM4_' + var], axis=1)
+            if var == 'pr':
+                ssp_df = ssp_df * 86400       # from kg/(m^2*s) to mm/day
+            return ssp_df
+
+    def process_dataframes(self):
+        """Separates the two scenarios and drops models not available for both scenarios and the historical period."""
+
+        ssp2 = self.df_ssp.loc[:, self.df_ssp.columns.str.startswith('ssp245')]
+        ssp5 = self.df_ssp.loc[:, self.df_ssp.columns.str.startswith('ssp585')]
+        hist = self.df_hist.loc[:, self.df_hist.columns.str.startswith('historical')]
+
+        ssp2.columns = ssp2.columns.str.lstrip('ssp245_').str.rstrip('_' + self.var)
+        ssp5.columns = ssp5.columns.str.lstrip('ssp585_').str.rstrip('_' + self.var)
+        hist.columns = hist.columns.str.lstrip('historical_').str.rstrip('_' + self.var)
+
+        common_models = set(ssp2.columns).intersection(ssp5.columns).intersection(hist.columns)
+
+        ssp2_common = ssp2.loc[:, common_models]
+        ssp5_common = ssp5.loc[:, common_models]
+        hist_common = hist.loc[:, common_models]
+
+        dropped_models = list(set([mod for mod in ssp2.columns if mod not in common_models] +
+                                  [mod for mod in ssp5.columns if mod not in common_models] +
+                                  [mod for mod in hist.columns if mod not in common_models]))
+
+        return ssp2_common, ssp5_common, hist_common, common_models, dropped_models
+
+    def get_results(self):
+        """Concatenates historical and scenario data to combined dataframes of the full downloaded period."""
+
+        ssp2_full = pd.concat([self.hist_common, self.ssp2_common])
+        ssp2_full.index.names = ['TIMESTAMP']
+        ssp5_full = pd.concat([self.hist_common, self.ssp5_common])
+        ssp5_full.index.names = ['TIMESTAMP']
+
+        return ssp2_full, ssp5_full
 
 
-def read_cmip(filename):
-    """Read files downloaded by download_cmip() and drop obsolete columns"""
-    df = pd.read_csv(filename)
-    df = df.drop(['system:index', '.geo', 'system:time_start'], axis=1)
-    df = df.rename(columns={'isodate': 'TIMESTAMP'})
-    return df
-
-def append_df(var, dir_path='.', hist=True):
-    df_list = []
-    if hist:
-        starty = 1979
-        endy = 2014
-    else:
-        starty = 2015
-        endy = 2100
-
-    for i in range(starty, endy+1):
-        filename = dir_path + '/cmip6_' + var + '_' + str(i) + '.csv'
-        df_list.append(read_cmip(filename))
-    if hist:
-        hist_df = pd.concat(df_list, ignore_index=True).drop('historical_GFDL-CM4_' + var, axis=1)
-        hist_df.columns = hist_df.columns.str.lstrip('historical_')
-        return hist_df
-    else:
-        ssp_df = pd.concat(df_list, ignore_index=True).drop(['ssp585_GFDL-CM4_' + var, 'ssp245_GFDL-CM4_' + var], axis=1)
-        return ssp_df
-
-
-append_df('tas', hist=False)
-
-
-
-# --> append respective scenario data with their historic data:
-    # - append_df
-    # - separate scenarios
-    # - remove ssp prefix
-    # - join both scenarios indidvidually with hist
-    # - save two dfs
-
-
+## Usage example
+processor = CMIPProcessor(dir=wd + '/ee_download_test', var='pr')
+ssp2_pr, ssp5_pr = processor.get_results()
+print(processor.dropped_models)
