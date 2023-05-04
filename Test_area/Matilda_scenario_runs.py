00d1808624700ca92fea2a76c9ad61f4a473c8b4
Phillip Schuster <phillip.schuster@posteo.de>
2023-05-05 00:29:48 +0200

    Finished bulk processing routine for  Matilda; added function to create custom df from bulk dict

--- Test_area/Matilda_scenario_runs.py
+++ Test_area/Matilda_scenario_runs.py
@@ -1,7 +1,6 @@
 import pickle
 import os
 import pandas as pd
-import numpy as np
 import sys
 from pathlib import Path
 from matilda.core import matilda_simulation
@@ -12,6 +11,7 @@ wd = home + '/EBA-CA/Papers/No1_Kysylsuu_Bash-Kaingdy/data'
 glacier_profile = pd.read_csv(wd + "/kyzulsuu_glacier_profile.csv")
 
 test_dir = '/home/phillip/Seafile/EBA-CA/Repositories/matilda_edu/output/cmip6/'
+
 
 def dict_to_pickle(dic, target_path):
     """
@@ -62,7 +62,7 @@ matilda_settings = {
     "set_up_start": '1979-01-01',  # Start date of the setup period
     "set_up_end": '1980-12-31',  # End date of the setup period
     "sim_start": '1981-01-01',  # Start date of the simulation period
-    "sim_end": '1990-12-31',  # End date of the simulation period
+    "sim_end": '2100-12-31',  # End date of the simulation period
     "freq": "M",  # Frequency of the data (monthly)
     "glacier_profile": glacier_profile,  # Glacier profile
     "area_cat": 295.763,  # Area of the catchment
@@ -106,70 +106,51 @@ param_dict = {
 tas = pickle_to_dict(test_dir + 'adjusted/tas.pickle')
 pr = pickle_to_dict(test_dir + 'adjusted/pr.pickle')
 
-## Write matilda scenario input to file
-
-
 ## Create MATILDA input
 
-matilda_settings = {
-    "set_up_start": '1979-01-01',  # Start date of the setup period
-    "set_up_end": '1980-12-31',  # End date of the setup period
-    "sim_start": '1981-01-01',  # Start date of the simulation period
-    "sim_end": '1990-12-31',  # End date of the simulation period
-    "freq": "M",  # Frequency of the data (monthly)
-    "glacier_profile": glacier_profile,  # Glacier profile
-    "area_cat": 295.763,  # Area of the catchment
-    "lat": 42.33,  # Latitude of the catchment
-    "warn": False,  # Warning flag
-    "plot_type": "all",  # Type of plot
-    "plots": False,  # Flag to indicate if plots should be generated
-    "elev_rescaling": True,  # Flag to indicate if elevation rescaling should be done
-    "ele_dat": 3172,  # Elevation of the data
-    "ele_cat": 3295,  # Elevation of the catchment
-    "area_glac": 32.51,  # Area of the glacier
-    "ele_glac": 4068,  # Elevation of the glacier
-    "pfilter": 0  # Filter parameter
-}
-param_dict = {
-    'lr_temp': -0.006077369,  # Lapse rate for temperature
-    'lr_prec': 0.0013269137,  # Lapse rate for precipitation
-    'BETA': 5.654754,
-    'CET': 0.08080378,
-    'FC': 365.68375,  # Field capacity
-    'K0': 0.36890236,  # K0 parameter
-    'K1': 0.022955153,  # K1 parameter
-    'K2': 0.060069658,  # K2 parameter
-    'LP': 0.63395154,  # LP parameter
-    'MAXBAS': 5.094901,  # Maximum basin storage
-    'PERC': 0.39491335,  # Percolation
-    'UZL': 348.0978,  # Upper zone limit
-    'PCORR': 1.0702422,  # Precipitation correction
-    'TT_snow': -1.1521467,  # Temperature threshold for snow
-    'TT_diff': 1.5895765,  # Temperature difference
-    'CFMAX_ice': 3.6518102,  # Maximum ice content
-    'CFMAX_rel': 1.8089349,  # Maximum relative content
-    'SFCF': 0.42293832,  # Soil field capacity
-    'CWH': 0.11234668,  # Crop water holding capacity
-    'AG': 0.9618855,
-    'RFS': 0.11432563  # Rainfall sensitivity ???
-}
+import pandas as pd
 
-# scenarios = {}
-# for s in [2, 5]:
-#     s = 'SSP' + str(s)
-#     scenarios[s] = {}
-#     for m in tas[s].columns:
-#         model = pd.DataFrame({'T2': tas[s][m],
-#                               'RRR': pr[s][m]})
-#         model = model.reset_index()
-#         mod_dict = {m: model.rename(columns={'time': 'TIMESTAMP'})}
-#         scenarios[s].update(mod_dict)
 
+def create_scenario_dict(tas: dict, pr: dict, scenario_nums: list) -> dict:
+    """
+    Create a nested dictionary of scenarios and models from two dictionaries of pandas DataFrames.
+    Parameters
+    ----------
+    tas : dict
+        A dictionary of pandas DataFrames where the keys are scenario names and each DataFrame has columns
+        representing different climate model mean daily temperature (K) time series.
+    pr : dict
+        A dictionary of pandas DataFrames where the keys are scenario names and each DataFrame has columns
+        representing different climate models mean daily precipitation (mm/day) time series.
+    scenario_nums : list
+        A list of integers representing the scenario numbers to include in the resulting dictionary.
+    Returns
+    -------
+    dict
+        A nested dictionary where the top-level keys are scenario names (e.g. 'SSP2', 'SSP5') and the values are
+        dictionaries containing climate models as keys and the corresponding pandas DataFrames as values.
+        The DataFrames have three columns: 'TIMESTAMP', 'T2', and 'RRR', where 'TIMESTAMP'
+        represents the time step, 'T2' represents the mean daily temperature (K), and 'RRR' represents the mean
+        daily precipitation (mm/day).
+    """
+    scenarios = {}
+    for s in scenario_nums:
+        s = 'SSP' + str(s)
+        scenarios[s] = {}
+        for m in tas[s].columns:
+            model = pd.DataFrame({'T2': tas[s][m],
+                                  'RRR': pr[s][m]})
+            model = model.reset_index()
+            mod_dict = {m: model.rename(columns={'time': 'TIMESTAMP'})}
+            scenarios[s].update(mod_dict)
+    return scenarios
+
+# scenarios = create_scenario_dict(tas, pr, [2, 5])
 # dict_to_pickle(scenarios, test_dir + 'adjusted/matilda_input.pickle')
 
-scenarios = pickle_to_dict( test_dir + 'adjusted/matilda_input.pickle')
+scenarios = pickle_to_dict(test_dir + 'adjusted/matilda_input.pickle')
 
-## Run Matilda in a loop
+## Run Matilda in a loop (takes a while - have a coffee)
 
 from tqdm import tqdm
 import contextlib
@@ -285,7 +266,9 @@ class MatildaBulkProcessor:
                 model_dict = {}  # Create an empty dictionary to store the model outputs
                 # Loop over the models with progress bar
                 model_list = [self.scenarios[scenario][m] for m in self.scenarios[scenario].keys()]
-                for model, model_output in zip(self.scenarios[scenario], pool.map(partial(self.matilda_headless, matilda_settings=self.matilda_settings, matilda_parameters=self.matilda_parameters), model_list)):
+                for model, model_output in zip(self.scenarios[scenario], pool.map(
+                        partial(self.matilda_headless, matilda_settings=self.matilda_settings,
+                                matilda_parameters=self.matilda_parameters), model_list)):
                     model_dict[model] = model_output
                 # Store the model dictionary in the scenario dictionary
                 out_dict[scenario] = model_dict
@@ -293,39 +276,102 @@ class MatildaBulkProcessor:
 
         return out_dict
 
-## Usage
+
+# Usage
 matilda_bulk = MatildaBulkProcessor(scenarios, matilda_settings, param_dict)
 # matilda_scenarios = matilda_bulk.run_single_process()
 matilda_scenarios = matilda_bulk.run_multi_process()
 
 dict_to_pickle(matilda_scenarios, test_dir + 'adjusted/matilda_scenarios.pickle')
 
+# matilda_scenarios = pickle_to_dict(test_dir + 'adjusted/matilda_scenarios.pickle')
 
-# matilda_scenarios['SSP2']['ACCESS-CM2']['model_output']['total_runoff']
+## Create custom dataframes for analysis
+
+def custom_df(dic, scenario, var, resample_freq=None):
+    """
+    Takes a dictionary of model outputs and returns a combined dataframe of a specific variable for a given scenario.
+    Parameters:
+        dic (dict): A nested dictionary of model outputs.
+                    The outer keys are scenario names and the inner keys are model names.
+                    The corresponding values are dictionaries containing two keys:
+                        'model_output' (DataFrame): containing model outputs for a given scenario and model
+                        'glacier_rescaling' (DataFrame): containing glacier properties for a given scenario and model
+        scenario (str): The name of the scenario to select from the dictionary.
+        var (str): The name of the variable to extract from the model output DataFrame.
+        resample_freq (str, optional): The frequency of the resulting time series data.
+                                       Defaults to None (i.e. no resampling).
+                                       If provided, should be in pandas resample frequency string format.
+    Returns:
+        pandas.DataFrame: A DataFrame containing the combined data of the specified variable for the selected scenario
+                          and models. The DataFrame is indexed by the time steps of the original models.
+                          The columns are the names of the models in the selected scenario.
+    Raises:
+        ValueError: If the provided  var  string is not one of the following: ['avg_temp_catchment', 'avg_temp_glaciers',
+                    'evap_off_glaciers', 'prec_off_glaciers', 'prec_on_glaciers', 'rain_off_glaciers', 'snow_off_glaciers',
+                    'rain_on_glaciers', 'snow_on_glaciers', 'snowpack_off_glaciers', 'soil_moisture', 'upper_groundwater',
+                    'lower_groundwater', 'melt_off_glaciers', 'melt_on_glaciers', 'ice_melt_on_glaciers', 'snow_melt_on_glaciers',
+                    'refreezing_ice', 'refreezing_snow', 'total_refreezing', 'SMB', 'actual_evaporation', 'total_precipitation',
+                    'total_melt', 'runoff_without_glaciers', 'runoff_from_glaciers', 'total_runoff', 'glacier_area',
+                    'glacier_elev', 'smb_water_year', 'smb_scaled', 'smb_scaled_capped', 'smb_scaled_capped_cum', 'surplus']
+    """
+    out1_cols = ['avg_temp_catchment', 'avg_temp_glaciers', 'evap_off_glaciers',
+                 'prec_off_glaciers', 'prec_on_glaciers', 'rain_off_glaciers',
+                 'snow_off_glaciers', 'rain_on_glaciers', 'snow_on_glaciers',
+                 'snowpack_off_glaciers', 'soil_moisture', 'upper_groundwater',
+                 'lower_groundwater', 'melt_off_glaciers', 'melt_on_glaciers',
+                 'ice_melt_on_glaciers', 'snow_melt_on_glaciers', 'refreezing_ice',
+                 'refreezing_snow', 'total_refreezing', 'SMB', 'actual_evaporation',
+                 'total_precipitation', 'total_melt', 'runoff_without_glaciers',
+                 'runoff_from_glaciers', 'total_runoff']
+
+    out2_cols = ['glacier_area', 'glacier_elev', 'smb_water_year',
+                 'smb_scaled', 'smb_scaled_capped', 'smb_scaled_capped_cum',
+                 'surplus']
+
+    if var in out1_cols:
+        output_df = 'model_output'
+    elif var in out2_cols:
+        output_df = 'glacier_rescaling'
+    else:
+        raise ValueError("var needs to be one of the following strings: " +
+                         str([out1_cols, out2_cols]))
+
+    # Create an empty list to store the dataframes
+    dfs = []
+    # Loop over the models in the selected scenario
+    for model in dic[scenario].keys():
+        # Get the dataframe for the current model
+        df = dic[scenario][model][output_df]
+        # Append the dataframe to the list of dataframes
+        dfs.append(df[var])
+    # Concatenate the dataframes into a single dataframe
+    combined_df = pd.concat(dfs, axis=1)
+    # Set the column names of the combined dataframe to the model names
+    combined_df.columns = dic[scenario].keys()
+    # Resample time series
+    if resample_freq is not None:
+        if output_df == 'glacier_rescaling':
+            if var in ['glacier_area', 'glacier_elev']:
+                combined_df = combined_df.resample(resample_freq).mean()
+            else:
+                combined_df = combined_df.resample(resample_freq).sum()
+        else:
+            if var in ['avg_temp_catchment', 'avg_temp_glaciers']:
+                combined_df = combined_df.resample(resample_freq).mean()
+            else:
+                combined_df = combined_df.resample(resample_freq).sum()
+
+    return combined_df
+
+
+custom_df(matilda_scenarios, scenario='SSP5', var='smb_water_year', resample_freq='Y')
 
 
 ## Plot example
 import matplotlib.pyplot as plt
-import pandas as pd
-# Define the scenario and column to plot
-scenario = 'SSP2'
-column_to_plot = 'total_runoff'
-output_no = 0
-resample_freq = 'Y'
-# Create an empty list to store the dataframes
-dfs = []
-# Loop over the models in the selected scenario
-for model in matilda_scenarios[scenario].keys():
-    # Get the dataframe for the current model
-    df = matilda_scenarios[scenario][model][output_no]
-    # Append the dataframe to the list of dataframes
-    dfs.append(df[column_to_plot])
-# Concatenate the dataframes into a single dataframe
-combined_df = pd.concat(dfs, axis=1)
-# Set the column names of the combined dataframe to the model names
-combined_df.columns = matilda_scenarios[scenario].keys()
-# Resample time series
-combined_df = combined_df.resample('Y').sum()
+
+combined_df = custom_df(matilda_scenarios, scenario='SSP5', var='runoff_from_glaciers', resample_freq='10Y')
 # Create the line plot
 combined_df.plot()
 plt.xlabel('x-axis label')
